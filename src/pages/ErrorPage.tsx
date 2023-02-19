436e2e93b941f09b767a92886b2fd96afc742c13
Spandan Barve <spandan567@gmail.com>
2023-02-19 13:09:24 +0530

    Styling for error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,6 +1,6 @@
 export default function ErrorPage({ error }: { error: string }) {
   return (
-    <div className="text-2xl font-semibold italic justify-center items-center">
+    <div className="text-2xl font-light italic justify-center items-center">
       {error}
     </div>
   );
